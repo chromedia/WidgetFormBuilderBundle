361e5d1aff0babc522093a01a212bfe08496008f
Floricel Colibao <floricel@Floricels-MacBook-Air.local>
2014-07-23 10:33:55 +0800

    Widget collection.

--- Resources/public/js/widgetFormBuilder.js
+++ Resources/public/js/widgetFormBuilder.js
@@ -22,49 +22,49 @@ var WidgetFormBuilder = function(options){
 };
 
 WidgetFormBuilder.prototype.initForm = function(){
-    
     var self = this;
 
-    // build widget choices
+    // TODO: Check if widget has choices
     var choicePrototype = this.formElements.widget_choices.data('prototype');
     this.formElements.widget_choices.append(choicePrototype);
+
+    this.form.on('click', '.add-widget-choice-trigger', function(e) {
+        e.preventDefault();
+
+        WidgetUtil.addRowInCollection(self.formElements.widget_choices);
+    });
 
     //build widget attributes
     var attributePrototype = this.formElements.widget_attribute.data('prototype');
     this.formElements.widget_attribute.append(attributePrototype);
 
-    //bind events
-    var addFormRowInCollection = function($_collectionHolder) {
-        var prototype = $_collectionHolder.data('prototype');
-        var index = $_collectionHolder.data('index');
-        var newForm = prototype.replace(/__name__/g, index);
+    this.form.on('click', '.add-widget-attribute-trigger', function(e) {
+        e.preventDefault();
 
-        $_collectionHolder.data('index', index + 1);
-        $_collectionHolder.append($(newForm));
-    }
+        WidgetUtil.addRowInCollection(self.formElements.widget_attribute);
+    });
 
-    this.form.on('click', '.remove-widget-choice-trigger', function(e) {
+    this.form.on('click', '.remove-widget-choice-trigger, .remove-widget-attribute-trigger', function(e) {
         e.preventDefault();
         $(this).closest('.form-group').hide().html('');
-    });
-
-    this.formElements.widget_choices.on('click', '.add-widget-choice-trigger', function(e) {
-        e.preventDefault();
-
-        self.formElements.widget_choices.data('index', self.formElements.widget_choices.find('.form-group').length);
-        addFormRowInCollection(self.formElements.widget_choices);
-    });
-
-    this.formElements.widget_attribute.on('click', '.add-widget-choice-trigger', function(e) {
-        e.preventDefault();
-
-        self.formElements.widget_attribute.data('index', self.formElements.widget_attribute.find('.form-group').length);
-
-        addFormRowInCollection(self.formElements.widget_attribute);
     });
 };
 
 
+// 
+var WidgetUtil = (function($) {
+    return {
+        addRowInCollection : function(collectionHolder) {
+            var prototype = collectionHolder.data('prototype');
+            var index = collectionHolder.find('.form-group').length;
+            var newForm = prototype.replace(/__name__/g, index);
+
+            collectionHolder.data('index', index + 1);
+            collectionHolder.append($(newForm));
+        }
+    }
+})(jQuery);
+                                                                                                                                                                                                                       
 
 
 
