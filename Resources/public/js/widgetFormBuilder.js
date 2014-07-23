e628454f0a018c9273ff3a2f27046b34f385686b
Floricel Colibao <floricel@Floricels-MacBook-Air.local>
2014-07-23 13:36:27 +0800

    Hide choices for widget without choices

--- Resources/public/js/widgetFormBuilder.js
+++ Resources/public/js/widgetFormBuilder.js
@@ -21,38 +21,50 @@ var WidgetFormBuilder = function(options){
     }
 };
 
+
 WidgetFormBuilder.prototype.initForm = function(){
+    this.bindEvents();
+
+    WidgetUtil.displayWidgetChoices(this.formElements.widget_id, this.formElements.widget_choices);
+
+    WidgetUtil.addRowInCollection(this.formElements.widget_choices);
+    WidgetUtil.addRowInCollection(this.formElements.widget_attribute);
+};
+
+
+WidgetFormBuilder.prototype.bindEvents = function(){
     var self = this;
 
-    // TODO: Check if widget has choices
-    // build widget choices
-    WidgetUtil.addRowInCollection(self.formElements.widget_choices);
     this.form.on('click', '.add-widget-choice-trigger', function(e) {
         e.preventDefault();
 
         WidgetUtil.addRowInCollection(self.formElements.widget_choices);
     });
 
-
-    // build widget attributes
-    WidgetUtil.addRowInCollection(self.formElements.widget_attribute);
     this.form.on('click', '.add-widget-attribute-trigger', function(e) {
         e.preventDefault();
 
         WidgetUtil.addRowInCollection(self.formElements.widget_attribute);
     });
 
-    // bind remove event
     this.form.on('click', '.remove-widget-choice-trigger, .remove-widget-attribute-trigger', function(e) {
         e.preventDefault();
         $(this).closest('.form-group').hide().html('');
     });
-};
+
+    this.formElements.widget_id.off('change').on('change', function(e) {
+        e.preventDefault();
+
+        WidgetUtil.displayWidgetChoices($(this), self.formElements.widget_choices);
+    });
+}
 
 
 // 
 var WidgetUtil = (function($) {
     return {
+        choiceWidgets : ['radio', 'choice', 'checkbox'],
+
         addRowInCollection : function(collectionHolder) {
             var prototype = collectionHolder.data('prototype');
             var index = collectionHolder.find('.form-group').length;
@@ -60,6 +72,18 @@ var WidgetUtil = (function($) {
 
             collectionHolder.data('index', index + 1);
             collectionHolder.append($(newForm));
+        }, 
+
+        displayWidgetChoices : function(widget, widget_choices) {
+            if (this.isChoiceWidget(widget.val())) {
+                widget_choices.closest('.form-group').show();
+            } else {
+                widget_choices.closest('.form-group').hide();
+            }
+        },
+
+        isChoiceWidget : function(widget) {
+            return $.inArray(widget, this.choiceWidgets) != -1;
         }
     }
 })(jQuery);
