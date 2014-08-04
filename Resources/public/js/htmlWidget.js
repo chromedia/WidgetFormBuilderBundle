24fae64034f25d260c1bd438799d81e544fda9f8
Floricel Colibao <floricel@Floricels-MacBook-Air.local>
2014-08-04 12:59:16 +0800

    Added widget for checkbox, textarea and radio button

--- Resources/public/js/htmlWidget.js
+++ Resources/public/js/htmlWidget.js
@@ -1,5 +1,9 @@
 var CWFB_HtmlWidget = function(metadata){
     
+    // //sample
+    // metadata = $.parseJSON('{"widget_id":"text","widget_choices":[],"widget_attribute":[],"widget_constraints":[]}');
+    // //sample
+
     this.widgetId = metadata.widget_id;
     this.widgetChoices = metadata.widget_choices;
     this.widgetAttributes = metadata.widget_attribute;
@@ -62,8 +66,92 @@ CWFB_ChoiceWidget.prototype._buildChoices = function() {
 //--- End choice widget
 
 
+//--- Checkbox widget
+var CWFB_CheckboxWidget = function(a, b) {
+    CWFB_HtmlWidget.call(this, a);
+};
+
+CWFB_CheckboxWidget.prototype = Object.create(CWFB_HtmlWidget.prototype);
+
+CWFB_CheckboxWidget.prototype._buildElement = function() {
+    this.el = document.createElement('span');
+};
+
+CWFB_CheckboxWidget.prototype._buildChoices = function() {
+    
+    var elDom = this.el;
+
+    this.widgetChoices.forEach(function(choice) {
+        var choice = document.createTextNode(choice);
+        var checkbox = document.createElement('input');
+
+        checkbox.type = 'checkbox';
+        checkbox.name = 'cwfb_checkbox';
+        checkbox.value = choice;
+        checkbox.appendChild(' '+choice);
+
+        elDom.appendChild(checkbox);
+    });
+
+    return this;
+};
+//--- End checkbox widget
+
+
+//--- Radio button widget
+var CWFB_RadioButtonWidget = function(a, b) {
+    CWFB_HtmlWidget.call(this, a);
+};
+
+CWFB_RadioButtonWidget.prototype = Object.create(CWFB_HtmlWidget.prototype);
+
+CWFB_RadioButtonWidget.prototype._buildElement = function() {
+    this.el = document.createElement('span');
+};
+
+CWFB_RadioButtonWidget.prototype._buildChoices = function() {
+    
+    var elDom = this.el;
+
+    this.widgetChoices.forEach(function(choice) {
+        var choice = document.createTextNode(choice);
+        var radioButton = document.createElement('input');
+
+        radioButton.type = 'radio';
+        radioButton.name = 'cwfb_radio';
+        radioButton.value = choice;
+        radioButton.appendChild(' '+choice);
+
+        elDom.appendChild(radioButton);
+    });
+
+    return this;
+};
+//--- End radio button widget
+
+
+//--- Text widget
 var CWFB_TextWidget = function(a, b) {
     CWFB_HtmlWidget.call(this, a);
 };
 
 CWFB_TextWidget.prototype = Object.create(CWFB_HtmlWidget.prototype);
+
+CWFB_TextWidget.prototype._buildElement = function() {
+    this.el = document.createElement('input');
+    this.el.type = 'text';
+};
+//--- End text widget
+
+
+//--- Textarea widget
+var CWFB_TextareaWidget = function(a, b) {
+    CWFB_HtmlWidget.call(this, a);
+};
+
+CWFB_TextareaWidget.prototype = Object.create(CWFB_HtmlWidget.prototype);
+
+CWFB_TextareaWidget.prototype._buildElement = function() {
+    this.el = document.createElement('textarea');
+};
+//--- End textarea widget
--- Resources/public/js/htmlWidgetFactory.js
+++ Resources/public/js/htmlWidgetFactory.js
@@ -12,7 +12,21 @@ var CWFB_HtmlWidgetFactory = (function($){
                     var widget = new CWFB_ChoiceWidget(metadata);
                     widget.build();
                     break;
+                case 'checkbox':
+                    var widget = new CWFB_CheckboxWidget(metadata);
+                    widget.build();
+                    break;
+                case 'radio':
+                    var widget = new CWFB_RadioButtonWidget(metadata);
+                    widget.build();
+                    break;
+                case 'textarea':
+                    var widget = new CWFB_TextareaWidget(metadata);
+                    widget.build();
+                    break;
                 case 'text':
+                    var widget = new CWFB_TextWidget(metadata);
+                    widget.build();
                     break;
                 default:
                     throw "CWFB_HtmlWidgetFactory: Unknown widget_id ["+metadata.widget_id+"]"
